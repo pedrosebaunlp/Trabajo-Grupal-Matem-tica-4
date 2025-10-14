5493a0cf300a2efb37b7f9b128b3df4809034681
PEDRO SEBA <pedrosebaunlp@gmail.com>
2025-10-14 17:04:38 -0300

    Avance

--- calcs/simple.py
+++ calcs/simple.py
@@ -1,8 +1,51 @@
-import numpy  as np
 import pandas as pd
+import numpy as np
 
-class Calculo:
-    def __init__(data):
-    """
-        Recibe un dataframe pandas
-    """
+def sxx(x,n):
+    sum_sq = (x**2).sum()
+    sq_sum = (x.sum())**2
+    return sum_sq - (sq_sum/n)
+
+def syy(y,n):
+    sum_sq = (y**2).sum()
+    sq_sum = (y.sum())**2
+    return sum_sq - (sq_sum/n)
+
+def sxy(x,y,n):
+    return ((x*y).sum())-(x.sum()*y.sum()/n)
+
+def ssr(x,y,n):
+    return syy(y,n)-((sxy(x,y,n)**2)/sxx(x,n))
+
+def beta_1_estim(x,y,n):
+    return sxy(x,y,n)/sxx(x,n)
+
+def beta_0_estim(x,y,n):
+    return y.mean()-(beta_1_estim(x,y,n)*x.mean())
+
+def varianza_estim(x,y,n):
+    return ssr(x,y,n)/(n-2)
+
+def R_cuadrado(x,y,n):
+    return 1 - (ssr(x,y,n)/syy(y,n))
+
+def r(x,y,n):
+    rsqr = R_cuadrado(x,y,n)
+    return np.sqrt(R_cuadrado(x,y,n))
+
+def y_estim(x,y,n):
+    return (beta_1_estim(x,y,n)*x)+beta_0_estim(x,y,n)
+
+def calculate(x,y,n):
+    print(f"""
+        \n Sxx:{sxx(x,n)},\n
+        \n Syy:{syy(y,n)},\n
+        \n Sxy:{sxy(x,y,n)},\n
+        \n B1:{beta_1_estim(x,y,n)},\n
+        \n B0:{beta_0_estim(x,y,n)},\n
+        \n y:{beta_1_estim(x,y,n)}x+({beta_0_estim(x,y,n)}),\n
+        \n SSr:{ssr(x,y,n)},\n
+        \n sigma^2:{varianza_estim(x,y,n)},\n
+        \n R2:{R_cuadrado(x,y,n)},\n
+        \n r:{r(x,y,n)}\n
+    """)
--- proyecto.py
+++ proyecto.py
@@ -1,16 +1,18 @@
-import calcs.multiple          as multiple
+#import calcs.multiple          as multiple
 import calcs.simple            as simple
 import matplotlib.pyplot       as plt
-
+import pandas as pd
 from ucimlrepo import fetch_ucirepo
 
 # Dataset obtenido desde: https://archive.ics.uci.edu/dataset/242/energy+efficiency
 energy_efficiency = fetch_ucirepo(id=242)
 
 # data (as pandas dataframes)
-X = energy_efficiency.data.features
-y = energy_efficiency.data.targets
-
-# Definimos como Var. Respuesta a Y1
-print(simple)
-
+X = energy_efficiency.data.features["X1"]
+Y = energy_efficiency.data.targets["Y1"]
+#n = X.size
+simple.calculate(X,Y,X.size)
+#print(X)
+# print(X.sum())
+# print((X**2).sum())
+# print(X.sum()**2)
