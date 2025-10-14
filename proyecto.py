ed211a6f1c0e5f6d04c80558291c327ea98d18c3
PEDRO SEBA <pedrosebaunlp@gmail.com>
2025-10-14 14:29:25 -0300

    Modulos para calculo simple y multiple

---
+++ proyecto.py
@@ -0,0 +1,15 @@
+import calcs.multiple          as multiple
+import calcs.simple            as simple
+import matplotlib.pyplot       as plt
+
+from ucimlrepo import fetch_ucirepo
+
+# Dataset obtenido desde: https://archive.ics.uci.edu/dataset/242/energy+efficiency
+energy_efficiency = fetch_ucirepo(id=242)
+
+# data (as pandas dataframes)
+X = energy_efficiency.data.features
+y = energy_efficiency.data.targets
+
+
+
