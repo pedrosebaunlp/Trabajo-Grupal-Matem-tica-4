f24da35c9eb42940f97b24eb66caf1cdbf71d651
Sofia Martinez Coria <sofiamartinezcoria@gmail.com>
2025-10-25 08:40:48 -0300

    Agrego los calculos de r, r2 , r2a y sigma

--- calcs/descenso_gradiente.py
+++ calcs/descenso_gradiente.py
@@ -1,6 +1,6 @@
 import numpy as np
 from ucimlrepo import fetch_ucirepo
-
+import multiple  #Las funciones para calculas r,r2,r2a y sigma al cuadrado
 # Dataset obtenido desde: https://archive.ics.uci.edu/dataset/242/energy+efficiency
 energy_efficiency = fetch_ucirepo(id=242)
 
@@ -8,36 +8,25 @@ Y = energy_efficiency.data.targets["Y1"]
 
 X = energy_efficiency.data.features.to_numpy()
 
-# Agregamos una columna de 1s para el intercepto (β0)
-m, n = X.shape
-X = np.column_stack([np.ones((m, 1)), X])  # ahora X tiene (m, n+1)
 
-# Inicialización
+m, n = X.shape
+X = np.column_stack([np.ones((m, 1)), X])  # Le agrega los 1 a la matriz
+
+
 beta = np.zeros(n + 1)
 alpha = 0.000003
-#0.0000003
-#0.000001
-epochs = 1000
+pasos = 1000 
 
-# Descenso del gradiente
-grad_norm_history = []
-for _ in range(epochs):
+# Intento de calculo del descenso del gradiente para estimar la reg mult
+#grad_norm_history = []
+for _ in range(pasos):
     y_pred = X @ beta
     error = y_pred - Y
     gradient = (1/m) * (X.T @ error)
     beta -= alpha * gradient
-    grad_norm = np.linalg.norm(gradient)  # magnitud del gradiente
-    grad_norm_history.append(grad_norm)
+    #grad_norm = np.linalg.norm(gradient)  
+    #grad_norm_history.append(grad_norm)
 
-
-
-
-import matplotlib.pyplot as plt
-plt.plot(grad_norm_history)
-plt.title("Norma del gradiente durante el entrenamiento")
-plt.xlabel("Iteraciones")
-plt.ylabel("||∇J(β)||")
-plt.savefig("reg2.png")
 
 print("Coeficientes β:")
 for i, b in enumerate(beta):
@@ -46,14 +35,18 @@ for i, b in enumerate(beta):
 b1 = beta[0]
 for i in range(1,9):
     b1+= beta[i] * energy_efficiency.data.features[f"X{i}"][0]
-print(b1)
+print("Estimacion de Y para el y1",b1) #Para ver si da un numero acorde 
 
 
-#Prod = beta @ X
+print("r ",multiple.r(energy_efficiency.data.features,Y))
+print("R2 ",multiple.R2(energy_efficiency.data.features,Y))
+print("R2a ",multiple.R2a(energy_efficiency.data.features,Y))
+print("sigma 2",multiple.sigma2(energy_efficiency.data.features,Y))
 
-"""
-plt.plot(cost_history)
-plt.title("Descenso del gradiente - Evolución del costo")
-plt.xlabel("Iteraciones")
-plt.ylabel("Costo J(β)")
-plt.savefig("reg.png")"""
+
+#import matplotlib.pyplot as plt
+#plt.plot(grad_norm_history)
+#plt.title("Norma del gradiente durante el entrenamiento")
+#plt.xlabel("Iteraciones")
+#plt.ylabel("||∇J(β)||")
+#plt.savefig("reg2.png")
